081ec94106a23c28ed80a80efc6122f41570634b
imhan14 <quanghannguyen14@gmail.com>
2023-01-04 20:21:37 +0700

    merge

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-require("dotenv").config();
+require("dotenv").config(); 
 
 const authRouter = require("./routes/auth.route");
 const userRouter = require("./routes/user.route");
